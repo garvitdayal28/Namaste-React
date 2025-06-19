e9e976827b21a39e305129dc0b0921a815107897
garvitdayal28 <garvitdayal17@gmail.com>
2025-06-19 12:47:40 +0530

    second-commit

---
+++ 1/script.js
@@ -0,0 +1,19 @@
+let heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World From React"
+);
+
+let parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Nested Element"),
+    React.createElement("h2", {}, "I am a h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm a h1 tag"),
+    React.createElement("h1", {}, "I'm a h1 tag"),
+  ]),
+]);
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
